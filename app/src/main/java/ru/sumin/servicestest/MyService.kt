7103754cf9a4cf81d1194b6855ad239914dceca0
Sergey <serglife777@gmail.com>
2021-11-29 13:55:50 +0200

    lifecycle service

--- app/src/main/java/ru/sumin/servicestest/MyService.kt
+++ app/src/main/java/ru/sumin/servicestest/MyService.kt
@@ -5,6 +5,22 @@ import android.content.Intent
 import android.os.IBinder
 
 class MyService : Service() {
+
+    // Service create
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    // work service
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    // service destroy
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
     override fun onBind(p0: Intent?): IBinder? {
         TODO("Not yet implemented")
     }
