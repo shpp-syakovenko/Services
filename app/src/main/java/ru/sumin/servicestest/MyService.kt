e19ca7e4002b00c70d46d0d73d46ea5f348acb65
Sergey <serglife777@gmail.com>
2021-11-29 13:48:31 +0200

    init

---
+++ app/src/main/java/ru/sumin/servicestest/MyService.kt
@@ -0,0 +1,11 @@
+package ru.sumin.servicestest
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+
+class MyService : Service() {
+    override fun onBind(p0: Intent?): IBinder? {
+        TODO("Not yet implemented")
+    }
+}
